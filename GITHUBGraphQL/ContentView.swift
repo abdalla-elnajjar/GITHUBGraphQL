4478f715a67c4204ba7563522f4aacfe761a99d7
abdalla-elnajjar <abdalla.elnajjar@clearbridgemobile.com>
2023-07-03 11:22:57 -0400

    Initial Commit

---
+++ GITHUBGraphQL/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GITHUBGraphQL
+//
+//  Created by Abdalla El Najjar on 2023-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GITHUBGraphQL/GITHUBGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITHUBGraphQLApp.swift
+//  GITHUBGraphQL
+//
+//  Created by Abdalla El Najjar on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct GITHUBGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
